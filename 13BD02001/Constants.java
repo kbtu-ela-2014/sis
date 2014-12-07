0a3c91f99b84eaa8108fdeb09e559b71d9609aa1
adema222 <adema.nurlina@gmail.com>
2014-12-07 20:31:01 +0600

    chat

---
+++ 13BD02001/Constants.java
@@ -0,0 +1,10 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Алимжан on 10.11.2014.
+ */
+public class Constants {
+    public final static String uri = "amqp://awnsofkt:js9uCN4-lfCDD3PHHBjlWhs9K7ib7t4z@turtle.rmq.cloudamqp.com/awnsofkt";
+    public final static String date = new SimpleDateFormat("[hh:mm:ss]").format(new Date());
+}
