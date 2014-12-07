c196995019be5ae0dc92ad2b08a611aa08c6994f
Akerke Baybakova <akerke.baybakova@gmail.com>
2014-12-07 21:55:55 +0400

    sis

---
+++ 12BD02046/src/gui/MyChatApp.java
@@ -0,0 +1,19 @@
+
+import gui.MainWindow;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author User
+ */
+public class MyChatApp {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindow screen = new MainWindow();
+        screen.show();
+    }
+}
